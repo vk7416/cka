054a5274d711159e7cfcc507ad9790fa3670df83
vk7416 <vkreddy7416@gmail.com>
2026-06-09 10:11:48 -0500

    update name of control3 to controlplane

--- setup-lb-ubuntu.sh
+++ setup-lb-ubuntu.sh
@@ -42,14 +42,14 @@ else
 fi
 
 
-if grep control3 /etc/hosts | grep -v 127
+if grep controlplane /etc/hosts | grep -v 127
 then
-        export CONTROL3_IP=$(awk '/control3/ { print $1 }' /etc/hosts | grep -v 127)
+        export controlplane_IP=$(awk '/controlplane/ { print $1 }' /etc/hosts | grep -v 127)
 else
-        echo enter IP address for control3
-        read CONTROL3_IP
-        export CONTROL3_IP=$CONTROL3_IP
-        sudo sh -c "echo $CONTROL3_IP control3 >> /etc/hosts"
+        echo enter IP address for controlplane
+        read controlplane_IP
+        export controlplane_IP=$controlplane_IP
+        sudo sh -c "echo $controlplane_IP controlplane >> /etc/hosts"
 fi
 
 
@@ -80,22 +80,22 @@ done
 ssh-keygen
 ssh-copy-id control1
 ssh-copy-id control2
-ssh-copy-id control3
+ssh-copy-id controlplane
 
 # configuring sudo for easier access
 sudo sh -c "echo 'Defaults timestamp_type=global,timestamp_timeout=60' >> /etc/sudoers"
 sudo scp -p /etc/sudoers $USER@control2:/tmp/ && ssh -t control2 'sudo -S chown root:root /tmp/sudoers' && ssh -t control2 'sudo -S cp -p /tmp/sudoers /etc/'
-sudo scp -p /etc/sudoers $USER@control3:/tmp/ && ssh -t control3 'sudo -S chown root:root /tmp/sudoers' && ssh -t control3 'sudo -S cp -p /tmp/sudoers /etc/'
+sudo scp -p /etc/sudoers $USER@controlplane:/tmp/ && ssh -t controlplane 'sudo -S chown root:root /tmp/sudoers' && ssh -t controlplane 'sudo -S cp -p /tmp/sudoers /etc/'
 #ssh control2 sudo -S sh -c "echo 'Defaults timestamp_type=global,timestamp_timeout=60' >> /etc/sudoers"
-#ssh control3 sudo -S sh -c "echo 'Defaults timestamp_type=global,timestamp_timeout=60' >> /etc/sudoers"
+#ssh controlplane sudo -S sh -c "echo 'Defaults timestamp_type=global,timestamp_timeout=60' >> /etc/sudoers"
 
 # install required software
 sudo apt install haproxy keepalived -y
 ssh control2 "sudo -S apt install haproxy keepalived -y"
-ssh control3 "sudo -S apt install haproxy keepalived -y"
+ssh controlplane "sudo -S apt install haproxy keepalived -y"
 
 scp /etc/hosts control2:/tmp && ssh -t control2 'sudo -S cp /tmp/hosts /etc/'
-scp /etc/hosts control3:/tmp && ssh -t control3 'sudo -S cp /tmp/hosts /etc/'
+scp /etc/hosts controlplane:/tmp && ssh -t controlplane 'sudo -S cp /tmp/hosts /etc/'
 
 # create keepalived config
 # change IP address to anything that works in your environment!
@@ -104,38 +104,38 @@ sudo cp check_apiserver.sh /etc/keepalived/
 
 
 scp check_apiserver.sh control2:/tmp && ssh -t control2 'sudo -S cp /tmp/check_apiserver.sh /etc/keepalived'
-scp check_apiserver.sh control3:/tmp && ssh -t control3 'sudo -S cp /tmp/check_apiserver.sh /etc/keepalived'
+scp check_apiserver.sh controlplane:/tmp && ssh -t controlplane 'sudo -S cp /tmp/check_apiserver.sh /etc/keepalived'
 
 #### creating site specific keepalived.conf file
 sudo cp keepalived.conf keepalived-control2.conf
-sudo cp keepalived.conf keepalived-control3.conf
+sudo cp keepalived.conf keepalived-controlplane.conf
 
 sudo sed -i 's/state MASTER/state SLAVE/' keepalived-control2.conf
-sudo sed -i 's/state MASTER/state SLAVE/' keepalived-control3.conf
+sudo sed -i 's/state MASTER/state SLAVE/' keepalived-controlplane.conf
 sudo sed -i 's/priority 255/priority 254/' keepalived-control2.conf
-sudo sed -i 's/priority 255/priority 253/' keepalived-control3.conf
+sudo sed -i 's/priority 255/priority 253/' keepalived-controlplane.conf
 
 sudo cp keepalived.conf /etc/keepalived/
 scp keepalived-control2.conf control2:/tmp && ssh -t control2 'sudo -S cp /tmp/keepalived-control2.conf /etc/keepalived/keepalived.conf'
-scp keepalived-control3.conf control3:/tmp && ssh -t control3 'sudo -S cp /tmp/keepalived-control3.conf /etc/keepalived/keepalived.conf'
+scp keepalived-controlplane.conf controlplane:/tmp && ssh -t controlplane 'sudo -S cp /tmp/keepalived-controlplane.conf /etc/keepalived/keepalived.conf'
 
 ### rewriting haproxy.cfg with site specific IP addresses
 sudo sed -i s/server\ control1\ 1.1.1.1\:6443\ check/server\ control1\ $CONTROL1_IP\:6443\ check/ haproxy.cfg
 sudo sed -i s/server\ control2\ 1.1.1.2\:6443\ check/server\ control2\ $CONTROL2_IP\:6443\ check/ haproxy.cfg
-sudo sed -i s/server\ control3\ 1.1.1.3\:6443\ check/server\ control3\ $CONTROL3_IP\:6443\ check/ haproxy.cfg
+sudo sed -i s/server\ controlplane\ 1.1.1.3\:6443\ check/server\ controlplane\ $controlplane_IP\:6443\ check/ haproxy.cfg
 
 # copy haproxy.cfg to destinations
 sudo cp haproxy.cfg /etc/haproxy/
 scp haproxy.cfg control2:/tmp && ssh -t control2 'sudo -S cp /tmp/haproxy.cfg /etc/haproxy/'
-scp haproxy.cfg control3:/tmp && ssh -t control3 'sudo -S cp /tmp/haproxy.cfg /etc/haproxy/'
+scp haproxy.cfg controlplane:/tmp && ssh -t controlplane 'sudo -S cp /tmp/haproxy.cfg /etc/haproxy/'
 
 # start and enable services
 sudo systemctl enable keepalived --now
 sudo systemctl enable haproxy --now
 ssh control2 sudo -S systemctl enable keepalived --now
 ssh control2 sudo -S systemctl enable haproxy --now
-ssh control3 sudo -S systemctl enable keepalived --now
-ssh control3 sudo -S systemctl enable haproxy --now
+ssh controlplane sudo -S systemctl enable keepalived --now
+ssh controlplane sudo -S systemctl enable haproxy --now
 
 echo setup is now done, please verify
 echo the first node that started the services - normally control1 -  should run the virtual IP address 192.168.29.100
